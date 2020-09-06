6311bc60553fd584e0275ffb99036b9a10d9937d
Lucas <47047328+Lucaslah@users.noreply.github.com>
2020-09-07 00:19:23 +1200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('INKSTATUS web server is working!!! CTR + C to quit on the command line.'); //write a response to the client
+  res.end(); //end the response
+}).listen(5000); 
+console.log("Go to http://localhost:5000 to see if the test is working.")
