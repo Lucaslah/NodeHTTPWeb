808967c9ccb20c60f644827fc18fc7ec04c1d7d4
Lucaslah <47047328+Lucaslah@users.noreply.github.com>
2021-03-14 09:47:24 +1300

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-
-var http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Node HTTP Web web server is working!!! CTR + C to quit on the command line.'); //write a response to the client
-  res.end(); //end the response
-}).listen(5000); 
-console.log("Go to http://localhost:5000 to see if the test is working.")
